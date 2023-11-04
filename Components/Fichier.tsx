aad5ed9eeee315454f30c99f8b6d788159b2363a
Gorhuann <theo.gueranger@gmail.com>
2023-11-04 16:05:23 +0100

    Fichier : Ajout du state open

--- Components/Fichier.tsx
+++ Components/Fichier.tsx
@@ -1,15 +1,43 @@
 "use client";
 
+import { useState } from "react";
+
 import style from "./Fichier.module.css";
+import Image from "next/image";
 
 export default function Fichier(props) {
+  const [open, setOpen] = useState(false);
+
   const handleClick = () => {
+    setOpen(!open);
     console.log("caca");
   };
 
-  return (
-    <fieldset className={style.ferme}>
-      <legend onClick={handleClick}>Fichier</legend>
-    </fieldset>
-  );
+  if (open) {
+    return (
+      <fieldset className={style.open}>
+        <legend onClick={handleClick}>
+          <Image
+            src="/IMG/Fichier/open.png"
+            alt="Fichie ouvert"
+            width={100}
+            height={100}
+          />
+        </legend>
+      </fieldset>
+    );
+  } else {
+    return (
+      <fieldset className={style.ferme}>
+        <legend onClick={handleClick}>
+          <Image
+            src="/IMG/Fichier/closed.png"
+            alt="Fichie fermé"
+            width={100}
+            height={100}
+          />
+        </legend>
+      </fieldset>
+    );
+  }
 }
