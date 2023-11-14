eb7fd746465555fb3c079db15f5faf714f2a3b36
Gorhuann <111096207+Gorhuann@users.noreply.github.com>
2023-11-14 13:50:36 +0100

    Page : Modification Style, display flex retiré

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,11 @@ export default function Home() {
   return (
     <main>
       <section>
-        <Fichier nomFichier="niveau 1"></Fichier>
+        <Fichier nomFichier="niveau 1">
+          <Fichier></Fichier>
+          <Fichier></Fichier>
+          <Fichier></Fichier>
+        </Fichier>
         <Fichier nomFichier="niveau 2"></Fichier>
         <Fichier nomFichier="niveau 3"></Fichier>
       </section>
