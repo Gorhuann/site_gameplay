882d07eca7ab97ce8db2df3823e633616f2abc4d
Gorhuann <111096207+Gorhuann@users.noreply.github.com>
2023-12-12 13:54:09 +0100

    Suite modif nom Fichier to Dossier

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Fichier from "@/Components/Dossier";
+import Dossier from "@/Components/Dossier";
 import "./page.css";
 
 export default function Home() {
   return (
     <main>
       <section>
-        <Fichier nomFichier="niveau 1">
-          <Fichier></Fichier>
-          <Fichier></Fichier>
-          <Fichier></Fichier>
-        </Fichier>
-        <Fichier nomFichier="niveau 2"></Fichier>
-        <Fichier nomFichier="niveau 3"></Fichier>
+        <Dossier nomFichier="niveau 1">
+          <Dossier></Dossier>
+          <Dossier></Dossier>
+          <Dossier></Dossier>
+        </Dossier>
+        <Dossier nomFichier="niveau 2"></Dossier>
+        <Dossier nomFichier="niveau 3"></Dossier>
       </section>
       <section>
         <p>caca</p>
