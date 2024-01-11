1b44c0ef16383ad15f2f4f2eef36e4c03ef10d8d
Gorhuann <theo.gueranger@gmail.com>
2024-01-11 23:23:10 +0100

    Lock : ajout du style

--- Components/Lock.tsx
+++ Components/Lock.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import style from "./Lock.module.css";
 
 export default function Lock(props) {
+  const [userCode, setUserCode] = useState("");
+
+  const handleClick = () => {
+    console.log(userCode);
+  };
+
   return (
     <div className={style.corps}>
       <div className={style.handle}>
-        <h3>Title</h3>
+        <h3>{props.nomLock}</h3>
         <div onClick={props.closeFile}>X</div>
       </div>
-      <div className={style.content}>This readme is really dragging on...</div>
+      <div className={style.content}>
+        <p>Pour ouvrir le dossier il vous faut un code !</p>
+        <input type="text" onChange={(e) => setUserCode(e.target.value)} />
+        <button onClick={handleClick}>Valider</button>
+      </div>
     </div>
   );
 }
